8508b80b3edf062ac740c4cd997067d17fa9a842
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-10-13 20:52:06 +0500

    Initialize UI with data from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import getScores from './module/getScores.js';
+import showScore from './module/showScore.js';
 import './style.css';
+
+window.onload = () => {
+  let gameData = [];
+  const loadInitialData = async () => {
+    gameData = await getScores();
+    showScore(gameData);
+  };
+  loadInitialData();
+};
